c33da58aa4bdd55071b2dc9b29b43ef4b3c2847e
Christian Anderson <tehninjawaffle@gmail.com>
2024-01-26 12:16:06 -0800

    Updated stylings

--- client/src/Components/navbar.js
+++ client/src/Components/navbar.js
@@ -1,23 +1,26 @@
 import { NavLink } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faLock } from "@fortawesome/free-solid-svg-icons"
+import { faLock, faExclamation } from "@fortawesome/free-solid-svg-icons"
 function Navbar () {
   return (
     <>
       <nav className="nav" >
-        <div class="iconContainer">
+        <div className="iconContainer">
           <NavLink to="/home" id="logo" className={({ active, pending, transitioning }) => [
             pending ? "pending" : "",
             active ? "active" : "",
             transitioning ? "transitioning": "",
-          ].join(" ")}><FontAwesomeIcon class="lockIcon" icon={faLock} /></NavLink>
+          ].join(" ")}><FontAwesomeIcon icon={faLock} size="2xl" style={{color: "whitesmoke",}} /></NavLink>
         </div>
-        <div class="linksContainer">
+        <div className="linksContainer">
           <NavLink to="/home" className="link">Home</NavLink>
           <NavLink to="/register" className="link">Register</NavLink>
           <NavLink to="/firstSecret" className="link">First Secret</NavLink>
           <NavLink to="/secondSecret" className="link">Second Secret</NavLink>
           <NavLink to="/thirdSecret" className="link">Third Secret</NavLink>
+        </div>
+        <div className="iconContainer">
+          <FontAwesomeIcon className="exclamIcon" icon={faExclamation} size="2xl" style={{color: "whitesmoke",}} />
         </div>
       </nav>
     </>
