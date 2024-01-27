24f04adc846ee3ad257c060bf931066c45556e1a
Christian Anderson <tehninjawaffle@gmail.com>
2024-01-26 17:46:53 -0800

    Missed some saves

--- client/src/Components/home/home.js
+++ client/src/Components/home/home.js
@@ -6,9 +6,10 @@
 
 function Home () {
   return (
-    <div>
-    <p>This will contain the Lock and Key, some animation for unlocking at after providing account details. Do it on Login?</p>
-    </div>
+    <>
+    
+    
+    </>
   )
 }
 
