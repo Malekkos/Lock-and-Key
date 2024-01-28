477b61a74e774c2d52d8e5ce9b0e0c96c2abac75
Christian Anderson <tehninjawaffle@gmail.com>
2024-01-27 16:31:18 -0800

    Got my locks and animations working for Home

--- client/src/Components/home/home.js
+++ client/src/Components/home/home.js
@@ -5,66 +5,91 @@ import { faLock, faUnlock } from "@fortawesome/free-solid-svg-icons"
 
 
 function Home () {
-// implement useEffect to check my token and see what perms it has
-// that will be deciding if the lock is already shown, or if it is unlocked
-
-  let firstButton = document.getElementById("firstButton")
-  let secondButton = document.getElementById("secondButton")
-  let thirdButton = document.getElementById("thirdButton")
-
-  // console.log(firstButton)
-  var tryMeKeys = [
-    { transform: 'translateY(0) scale(1, 1) rotate(0)', easing: 'ease-in' },
-    { transform: 'translateY(0) scale(1.1, .9) rotate(0)' },
-    { transform: 'translateY(-10%) scale(.9, 1.1) rotate(0)', offset: .4 },
-    { transform: 'translateY(-10%) scale(1, 1) rotate(10deg)', offset: .5 },
-    { transform: 'translateY(-10%) scale(1, 1) rotate(-10deg)', offset: .7 },
-    { transform: 'translateY(-10%) scale(1,1) rotate(0deg)', offset: .8, easing: 'ease-in' },
-    { transform: 'translateY(0) scale(1, 1) rotate(0)' }
-  ];
-
-  let clickVerify = function() {
-    console.log("working")
-    firstButton.animate( tryMeKeys,
-      {transform: "scale(1,1) rotate(20deg)"})
-  }
-
-
+  // implement useEffect to check my token and see what perms it has
+  // that will be deciding if the lock is already shown, or if it is unlocked
+  // dom
   const [locked, setLocked] = useState(true)
   const [unlocked, setUnlocked] = useState(true)
-  async function onClick (token, lockType) { //probably has token?
+
+  window.onload = ("DOMContentLoaded", event => {
+    let firstButton = document.getElementById("firstButton")
+    let secondButton = document.getElementById("secondButton")
+    let thirdButton = document.getElementById("thirdButton")
+
+    let firstLock = document.getElementById("firstLock")
+    let secondLock = document.getElementById("secondLock")
+    let thirdLock = document.getElementById("thirdLock")
+    let fourthLock = document.getElementById("fourthLock")
+    let fifthLock = document.getElementById("fifthLock")
+    let sixthLock = document.getElementById("sixthLock")
+
+    let clickVerify = [
+      { transform: "scale(1)" },
+      { transform: "scale(2)" },
+      { transform: "scale(1)" }
+    ]
+    let alreadyUnlocked = [
+      {transform: "rotate(0deg)"},
+      {transform: "rotate(-20deg)"},
+      {transform: "rotate(20deg)"},
+      {transform: "rotate(0deg)"}
+
+    ]
+
+    let timing = {
+      duration: 1000,
+      iterations: 1,
+    }
+
+    firstButton.addEventListener("click", () => {
+      firstLock.animate(clickVerify, timing)
+    }, false)
+    firstButton.addEventListener("click", () => {
+      secondLock.animate(alreadyUnlocked, timing)
+    }, false)
+    secondButton.addEventListener("click", () => {
+      thirdLock.animate(clickVerify, timing)
+    }, false)
+    secondButton.addEventListener("click", () => {
+      fourthLock.animate(alreadyUnlocked, timing)
+    }, false)
+    thirdButton.addEventListener("click", () => {
+      fifthLock.animate(clickVerify, timing)
+    }, false)
+    thirdButton.addEventListener("click", () => {
+      sixthLock.animate(alreadyUnlocked, timing)
+    }, false)
+  })
+
+
+  //Create an error message for the if statement in onClick, or make a error message hidden until done
+
+  function onClick (token, lockType) { //probably has token?
     let lock = document.getElementsByName(lockType)
-    console.log(token)
     lock[1].classList.forEach(val => {
       if(val === "locked" && token) {
-        console.log(val)
-        lock[0].classList.add("locked")
-        lock[1].classList.remove("locked")
-        console.log(lock[1].classList)
+        setTimeout(() => lock[0].classList.add("locked"), 1000)
+        setTimeout(() => lock[1].classList.remove("locked"), 1000)
       }
     })
   }
-
-  // firstButton.addEventListener("click", clickVerify)
-
-
   return (
     <>
       <div className="main">
         <div className="secretsContainer">
           <div id="firstSecret">
-            <FontAwesomeIcon name="bronzeLock" className="unlocked bronzeLock" icon={faLock} size="2xl" />
-            <FontAwesomeIcon name="bronzeLock" className="locked bronzeLock" icon={faUnlock} size="2xl" />
+            <FontAwesomeIcon id="firstLock" name="bronzeLock" className="unlocked bronzeLock" icon={faLock} size="2xl" />
+            <FontAwesomeIcon id="secondLock" name="bronzeLock" className="locked bronzeLock" icon={faUnlock} size="2xl" />
             <input type="button" id="firstButton" className="verifyBtn" onClick={() => onClick("fgsfger", "bronzeLock")} value="Verify"></input>
           </div>
           <div id="secondSecret">
-            <FontAwesomeIcon name="silverLock" className="unlocked silverLock" icon={faLock} size="2xl" />
-            <FontAwesomeIcon name="silverLock" className="locked silverLock" icon={faUnlock} size="2xl" />
+            <FontAwesomeIcon id="thirdLock" name="silverLock" className="unlocked silverLock" icon={faLock} size="2xl" />
+            <FontAwesomeIcon id="fourthLock" name="silverLock" className="locked silverLock" icon={faUnlock} size="2xl" />
             <input type="button" id="secondButton" className="verifyBtn" onClick={() => onClick("dasf", "silverLock")} value="Verify"></input>
           </div>
           <div id="thirdSecret">
-            <FontAwesomeIcon name="goldLock" className="unlocked goldLock" icon={faLock} size="2xl" />
-            <FontAwesomeIcon name="goldLock" className="locked goldLock" icon={faUnlock} size="2xl" />
+            <FontAwesomeIcon id="fifthLock" name="goldLock" className="unlocked goldLock" icon={faLock} size="2xl" />
+            <FontAwesomeIcon id="sixthLock" name="goldLock" className="locked goldLock" icon={faUnlock} size="2xl" />
             <input type="button" id="thirdButton" className="verifyBtn" onClick={() => onClick("asdfasdf", "goldLock")} value="Verify"></input>
           </div>
         </div>
