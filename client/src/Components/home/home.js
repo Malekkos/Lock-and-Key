c6b36e976d4d62b46bfa7a54337e39fc0f630190
Christian Anderson <tehninjawaffle@gmail.com>
2024-02-03 14:19:15 -0800

    Login now logs in and hides all the login stuff. Small issue, there is no token in cookies, I forgot to have it set

--- client/src/Components/home/home.js
+++ client/src/Components/home/home.js
@@ -8,24 +8,11 @@ import axios from "axios"
 
 function Home () {
   const [loggedIn, setLoggedIn] = useState(false)
-  const [errorMessage, setErrorMessage] = useState()
+  const [verifyErrorMessage, setVerifyErrorMessage] = useState()
   const [creds, setCreds ] = useState({username: "", password: ""})
   const [greetingMessage, setGreetingMessage] = useState()
   
 
-  //THOUGHTS: Maybe set a timer on greetingMessage
-  function callServer() {
-    axios.post("http://localhost:9000/api/auth/login", creds)
-    .then(res => {
-      setLoggedIn(true)
-      setGreetingMessage(res.data.message)
-      setCreds({username: "", password: ""})
-      console.log(res)
-    })
-    .catch(err => {
-      console.log(err.response.data.message)
-    })
-  }
 
   //THOUGHTS: I should move the animation to a separate folder, probably called animation
   let onLoad = window.onload = ("DOMContentLoaded", event => {
@@ -82,8 +69,6 @@ function Home () {
   // implement useEffect to check my token and see what perms it has
   // that will be deciding if the lock is already shown, or if it is unlocked
   let onChange = (event, type) => {
-    // console.log(event)
-    // console.log(type)
     console.log(creds.username)
     if(type === "username") {
       setCreds({username: event, password: creds.password})
@@ -92,10 +77,21 @@ function Home () {
     }
   }
 
+  //THOUGHTS: Maybe set a timer on greetingMessage
   let onSubmit = event => {
     // axios.post() server needs to be built first
     event.preventDefault()
-    setLoggedIn(true)
+    axios.post("http://localhost:9000/api/auth/login", creds)
+    .then(res => {
+      setLoggedIn(true)
+      setVerifyErrorMessage("")
+      setGreetingMessage(res.data.message)
+      setCreds({username: "", password: ""})
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err.response.data.message)
+    })
   }
 
   useEffect(() => {
@@ -106,14 +102,15 @@ function Home () {
     let lock = document.getElementsByName(lockType)
     let error = document.getElementById(`errorMessage${lockNumber}`)
     if(loggedIn === false) {
-      setErrorMessage("You need to login first. That's authentication!")
+      setVerifyErrorMessage("You need to login first. That's authentication!")
       error.classList.remove("locked")
     } else if (lock[1].classList.contains("locked") && token /*have to find out how to test token, think the testing script is for servers only. TBD*/) {
-      setErrorMessage("")
+      setVerifyErrorMessage("")
       setTimeout(() => lock[0].classList.add("locked"), 1000)
       setTimeout(() => lock[1].classList.remove("locked"), 1000)
     }
   }
+
   return (
     <>
       <div className="main">
@@ -122,21 +119,21 @@ function Home () {
             <FontAwesomeIcon id="firstLock" name="bronzeLock" className="unlocked bronzeLock" icon={faLock} size="2xl" />
             <FontAwesomeIcon id="secondLock" name="bronzeLock" className="locked bronzeLock" icon={faUnlock} size="2xl" />
             <input type="button" id="firstButton" className="verifyBtn" onClick={() => onClick("fgsfger", "bronzeLock", "One")} value="Verify"></input>
-            <p id="errorMessageOne" className="errorMessage locked">{errorMessage}</p>
+            <p id="errorMessageOne" className="errorMessage locked">{verifyErrorMessage}</p>
             <p className="locked">{/*this data will be from a get call, made with axios(most likely). Should have error message here if call fails(eg. no token)*/}</p>
           </div>
           <div id="secondSecret">
             <FontAwesomeIcon id="thirdLock" name="silverLock" className="unlocked silverLock" icon={faLock} size="2xl" />
             <FontAwesomeIcon id="fourthLock" name="silverLock" className="locked silverLock" icon={faUnlock} size="2xl" />
             <input type="button" id="secondButton" className="verifyBtn" onClick={() => onClick("dasf", "silverLock", "Two")} value="Verify"></input>
-            <p id="errorMessageTwo" className="errorMessage locked">{errorMessage}</p>
+            <p id="errorMessageTwo" className="errorMessage locked">{verifyErrorMessage}</p>
             <p className="locked">{/*this data will be from a get call, made with axios(most likely). Should have error message here if call fails(eg. no token)*/}</p>
           </div>
           <div id="thirdSecret">
             <FontAwesomeIcon id="fifthLock" name="goldLock" className="unlocked goldLock" icon={faLock} size="2xl" />
             <FontAwesomeIcon id="sixthLock" name="goldLock" className="locked goldLock" icon={faUnlock} size="2xl" />
             <input type="button" id="thirdButton" className="verifyBtn" onClick={() => onClick("asdfasdf", "goldLock", "Three")} value="Verify"></input>
-            <p id="errorMessageThree" className="errorMessage locked">{errorMessage}</p>
+            <p id="errorMessageThree" className="errorMessage locked">{verifyErrorMessage}</p>
             <p className="locked">{/*this data will be from a get call, made with axios(most likely). Should have error message here if call fails(eg. no token)*/}</p>
           </div>
         </div>
@@ -144,11 +141,12 @@ function Home () {
           <h2 className="homeHeader">Thanks for visiting!</h2>
           <div className="mainInstructions">
             <p className="homeText">This is a demo project to showcase my understanding of <span className="authMods">Authentication</span> & <span className="authMods">Authorization</span></p>
-            <p className="homeText">Complete some, ultimately arbitrary, tasks and be rewarded with a few of my secrets!</p>
-          </div>
-        <form onSubmit={(event) => onSubmit(event)} className="loginContainer">
-          <div className="inputsContainer">
+            <p className="homeText">Complete some arbitrary tasks and be rewarded with a few of my secrets!</p>
             <p className="authMods">{greetingMessage}</p>
+          </div>
+    { loggedIn ? "" : <form onSubmit={(event) => onSubmit(event)} className="loginContainer">
+        <div className="inputsContainer">
+            
             <div className="passContainer">
               <h4 className="homeText">Username</h4>
               <input onChange={(event) => onChange(event.target.value, "username")} className="loginUsername" value={creds.username} type="text" />
@@ -157,9 +155,9 @@ function Home () {
               <h4 className="homeText">Password</h4>
               <input onChange={(event) => onChange(event.target.value, "password")} className="loginPassword" value={creds.password} type="password" />
             </div>
-          <input onClick={() => callServer()} type="submit" className="authMods" value="Login" />
+            <input type="submit" className="authMods" value="Login" /> 
           </div>
-        </form>
+        </form>}
         </div>
       </div>
     </>
