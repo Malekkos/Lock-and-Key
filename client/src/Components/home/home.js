27dffc818d2db20ca027983516b306c2d1f78d11
Christian Anderson <tehninjawaffle@gmail.com>
2024-02-03 21:57:33 -0800

    Added error checker to better track error messages rather then multiple uses of state

--- client/src/Components/home/home.js
+++ client/src/Components/home/home.js
@@ -7,13 +7,13 @@ import axios from "axios"
 
 
 function Home () {
+  const initialError = {errMessageOne: "", errMessageTwo: "", errMessageThree: "", errMessageFour: ""}
   const [loggedIn, setLoggedIn] = useState(false)
-  const [verifyErrorMessage, setVerifyErrorMessage] = useState()
+  const [errorMessage, setErrorMessage] = useState(initialError)
   const [creds, setCreds ] = useState({username: "", password: ""})
   const [greetingMessage, setGreetingMessage] = useState()
+  //THOUGHTS: Should have a method to check if token exists, and then omit the login place
   
-
-
   //THOUGHTS: I should move the animation to a separate folder, probably called animation
   let onLoad = window.onload = ("DOMContentLoaded", event => {
     console.log('loaded')
@@ -84,7 +84,7 @@ function Home () {
     axios.post("http://localhost:9000/api/auth/login", creds)
     .then(res => {
       setLoggedIn(true)
-      setVerifyErrorMessage("")
+      setErrorMessage("")
       setGreetingMessage(res.data.message)
       setCreds({username: "", password: ""})
       console.log(res)
@@ -100,15 +100,33 @@ function Home () {
 
   function onClick (token, lockType, lockNumber) { //probably has token?
     let lock = document.getElementsByName(lockType)
-    let error = document.getElementById(`errorMessage${lockNumber}`)
+    let error = document.getElementById(`errMessage${lockNumber}`)
     if(loggedIn === false) {
-      setVerifyErrorMessage("You need to login first. That's authentication!")
+      errorChecker(error)
       error.classList.remove("locked")
     } else if (lock[1].classList.contains("locked") && token /*have to find out how to test token, think the testing script is for servers only. TBD*/) {
-      setVerifyErrorMessage("")
+      setErrorMessage("")
       setTimeout(() => lock[0].classList.add("locked"), 1000)
       setTimeout(() => lock[1].classList.remove("locked"), 1000)
     }
+  }
+
+  // Function that takes in the errorNum(to be specific) and assigns it to the place it needs to go
+  function errorChecker(errorNum) {
+    // console.log(errorNum.textContent)
+    let currError = errorNum.id
+    console.log(typeof(currError))
+    setErrorMessage({
+      ...errorMessage, 
+      [currError]: "adfsasdfwer"
+    })
+    // setErrorMessage()
+    console.log(errorMessage)
+    // setErrorMessage(errorMessage.errorNum = "farty hardy")
+    // define an object in state containing all possible errors.
+    // Have said errors in the return, like under errorMessageOne
+    // When a new error comes up, wipe other errors
+
   }
 
   return (
@@ -119,21 +137,21 @@ function Home () {
             <FontAwesomeIcon id="firstLock" name="bronzeLock" className="unlocked bronzeLock" icon={faLock} size="2xl" />
             <FontAwesomeIcon id="secondLock" name="bronzeLock" className="locked bronzeLock" icon={faUnlock} size="2xl" />
             <input type="button" id="firstButton" className="verifyBtn" onClick={() => onClick("fgsfger", "bronzeLock", "One")} value="Verify"></input>
-            <p id="errorMessageOne" className="errorMessage locked">{verifyErrorMessage}</p>
+            <p id="errMessageOne" className="errorMessage locked">{errorMessage.errMessageOne}</p>
             <p className="locked">{/*this data will be from a get call, made with axios(most likely). Should have error message here if call fails(eg. no token)*/}</p>
           </div>
           <div id="secondSecret">
             <FontAwesomeIcon id="thirdLock" name="silverLock" className="unlocked silverLock" icon={faLock} size="2xl" />
             <FontAwesomeIcon id="fourthLock" name="silverLock" className="locked silverLock" icon={faUnlock} size="2xl" />
             <input type="button" id="secondButton" className="verifyBtn" onClick={() => onClick("dasf", "silverLock", "Two")} value="Verify"></input>
-            <p id="errorMessageTwo" className="errorMessage locked">{verifyErrorMessage}</p>
+            <p id="errMessageTwo" className="errorMessage locked">{errorMessage.errMessageTwo}</p>
             <p className="locked">{/*this data will be from a get call, made with axios(most likely). Should have error message here if call fails(eg. no token)*/}</p>
           </div>
           <div id="thirdSecret">
             <FontAwesomeIcon id="fifthLock" name="goldLock" className="unlocked goldLock" icon={faLock} size="2xl" />
             <FontAwesomeIcon id="sixthLock" name="goldLock" className="locked goldLock" icon={faUnlock} size="2xl" />
             <input type="button" id="thirdButton" className="verifyBtn" onClick={() => onClick("asdfasdf", "goldLock", "Three")} value="Verify"></input>
-            <p id="errorMessageThree" className="errorMessage locked">{verifyErrorMessage}</p>
+            <p id="errMessageThree" className="errorMessage locked">{errorMessage.errMessageThree}</p>
             <p className="locked">{/*this data will be from a get call, made with axios(most likely). Should have error message here if call fails(eg. no token)*/}</p>
           </div>
         </div>
@@ -146,7 +164,7 @@ function Home () {
           </div>
     { loggedIn ? "" : <form onSubmit={(event) => onSubmit(event)} className="loginContainer">
         <div className="inputsContainer">
-            
+            <p id="errorMessageFour" className="errorMessage locked">{errorMessage.errMessageFour}</p>
             <div className="passContainer">
               <h4 className="homeText">Username</h4>
               <input onChange={(event) => onChange(event.target.value, "username")} className="loginUsername" value={creds.username} type="text" />
