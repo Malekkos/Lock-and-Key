4a999f143e5c576c03c72ded1b7c62d89c9e680c
Christian Anderson <tehninjawaffle@gmail.com>
2024-02-15 04:09:27 -0800

    Added the format for the register page as well as some styling

--- client/src/Components/register/register.js
+++ client/src/Components/register/register.js
@@ -1,13 +1,38 @@
 
-
-
+import React, { useState } from "react"
+import axios from "axios"
 
 function Register () {
 
+  const initialCreds = {username: "", password: ""}
+  const [creds, setCreds] = useState(initialCreds)
+
+  // const onChange = event => {
+// 
+  // }
   return (
-    <div>
-      <p>Option for creating a new account</p>
+    <>
+    <div className="regMain">
+      <div className="regContainer">
+       <div>
+          <p>Create a new account to get started!</p>
+          <p>You'll be giving a variety of challenges</p>
+          <p>to complete, with the purposes of increasing</p>
+          <p>your user privilege.</p>
+        </div>
+        <div className="regCredsContainer">
+          <div className="regUsername">
+            <p className="regUsernameText">Username</p>
+            <input className="regInput" type="text" />
+          </div>
+          <div className="regPassword">
+            <p className="regPasswordText">Password</p>
+            <input className="regInput" type="password" />
+          </div>
+        </div>
+      </div>
     </div>
+    </>
   )
 }
 
