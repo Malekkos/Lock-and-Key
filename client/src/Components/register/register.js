1f54f93448dbb88c2cd8b95e72f85bc18dc9c2f3
Christian Anderson <tehninjawaffle@gmail.com>
2024-02-15 23:55:13 -0800

    Did a good deal of work, can hit the register end point and it does make the account, Noticable issue thought. Its accepting blank usernames. Thats not supposed to happen. Also, a few other things, logging in should set a message to say 'welcome, so and so', as well as redirect to the homepage on a delay

--- client/src/Components/register/register.js
+++ client/src/Components/register/register.js
@@ -2,36 +2,65 @@
 import React, { useState } from "react"
 import axios from "axios"
 
-function Register () {
-
-  const initialCreds = {username: "", password: ""}
+function Register() {
+  const initialError = ""
+  const initialCreds = { username: "", password: "" }
   const [creds, setCreds] = useState(initialCreds)
+  const [error, setError] = useState(initialError)
 
-  // const onChange = event => {
-// 
-  // }
+  const onChange = (event, type) => {
+    event.preventDefault()
+
+    if (type === "username") {
+      setCreds({ username: event.target.value, password: creds.password })
+    } else {
+      setCreds({ username: creds.username, password: event.target.value })
+    }
+    console.log(creds)
+  }
+
+  const onSubmit = event => {
+    event.preventDefault()
+    axios.post("http://localhost:9000/api/auth/register", creds)
+    .then(res => {
+      console.log(res)
+      setError(initialError)
+      setCreds(initialCreds)
+    })
+    .catch(err => {
+      console.log(err.response.data.message)
+      setError(err.response.data.message)
+    })
+  }
+
   return (
     <>
-    <div className="regMain">
-      <div className="regContainer">
-       <div>
-          <p>Create a new account to get started!</p>
-          <p>You'll be giving a variety of challenges</p>
-          <p>to complete, with the purposes of increasing</p>
-          <p>your user privilege.</p>
-        </div>
-        <div className="regCredsContainer">
-          <div className="regUsername">
-            <p className="regUsernameText">Username</p>
-            <input className="regInput" type="text" />
+      <div className="regMain">
+        <div className="regContainer">
+          <div className="regInstructions">
+            <p className="regInstrText">Create a new account to get started!</p>
+            <p className="regInstrText">You'll be giving a variety of challenges</p>
+            <p className="regInstrText">to complete, with the purposes of</p>
+            <p className="regInstrText">increasing your user privilege</p>
           </div>
-          <div className="regPassword">
-            <p className="regPasswordText">Password</p>
-            <input className="regInput" type="password" />
-          </div>
+          { error ? <p className="errorMessage">{error}</p> : "" }
+          <form className="reg" onSubmit={event => onSubmit(event)}>
+            <div className="regCredsContainer">
+              <div className="regUsername">
+                <p className="regUsernameText">Username</p>
+                <input className="regInput" type="text" onChange={event => onChange(event, "username")} value={creds.username} />
+              </div>
+              <div className="regPassword">
+                <p className="regPasswordText">Password</p>
+                <input className="regInput" type="password" onChange={event => onChange(event, "password")} value={creds.password} />
+              </div>
+              <div className="submit">
+                <input className="regButton" type="submit" value="Register" />
+              </div>
+            </div>
+          </form>
         </div>
       </div>
-    </div>
     </>
   )
 }
