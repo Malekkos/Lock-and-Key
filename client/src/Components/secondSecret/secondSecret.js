1fe152f42ae51bfcd2970589993c2a20792e0259
Christian Anderson <tehninjawaffle@gmail.com>
2024-03-15 02:24:59 -0700

    Changed second secret to functionally work, just with no content. Still not sure what the game should be

--- client/src/Components/secondSecret/secondSecret.js
+++ client/src/Components/secondSecret/secondSecret.js
@@ -1,10 +1,33 @@
 
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+
 
 
 
 function SecondSecret() {
+
+  let [message, setMessage] = useState({message: ""})
+
+  let onclick = (event) => {
+    axios.put("http://localhost:9000/api/auth/increase", { "username": localStorage.getItem("username") })
+    .then(res => {
+      setMessage(res.data)
+      localStorage.setItem("tokenToBePassed", res.data.token)
+    })
+    .catch(error => {
+      setMessage(error)
+    })
+  }
+
   return (
-    <div>some secret, TBD</div>
+    <>
+    <div className="gameTwoMain">
+      { message ? <b>{message.message}</b> : "" }
+      <p className="gameTwoText"> This is a <i className="gameTwoText">Temporary</i> placeholder button strictly for testing purposes, in lieu of the game thats supposed to be here. The purpose is increasing user perms</p>
+      <button className="gameTwoButton" onClick={(event) => onclick(event)}>Click Me!</button>
+    </div>
+    </>
   )
 }
 
