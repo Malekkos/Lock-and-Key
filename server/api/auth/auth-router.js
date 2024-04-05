7edc7a405225453e5d8381307a47a5a358523636
Christian Anderson <tehninjawaffle@gmail.com>
2024-04-05 08:00:54 -0700

    More testing...

--- server/api/auth/auth-router.js
+++ server/api/auth/auth-router.js
@@ -6,6 +6,12 @@ const { JWT_SECRET, BCRYPT_ROUNDS } = require("../secrets")
 
 const Users = require("../users/users-model")
 
+router.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 //DESC: Method to create an account
 router.post("/register", usernameTaken, async (req, res, next) => {
   let { username, password } = req.body
