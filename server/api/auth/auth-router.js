c05b8c65cf136880384a4aa287a7cbaee78ddb10
Christian Anderson <tehninjawaffle@gmail.com>
2024-04-05 08:03:35 -0700

    More testing..

--- server/api/auth/auth-router.js
+++ server/api/auth/auth-router.js
@@ -7,6 +7,7 @@ const { JWT_SECRET, BCRYPT_ROUNDS } = require("../secrets")
 const Users = require("../users/users-model")
 
 router.use((req, res, next) => {
+  res.append("content-length", 123)
   res.append('Access-Control-Allow-Origin', ['*']);
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.append('Access-Control-Allow-Headers', 'Content-Type');
