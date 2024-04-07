e76ec6c6613243cdb20d9cf98be78a347cdff1ec
Christian Anderson <tehninjawaffle@gmail.com>
2024-04-07 10:57:41 -0700

    Hail Mary 9

--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ const port = process.env.PORT || 9000
 // cont. : a specific req on them?
 
 // Simple launching point for server to start
-server.use((req, res, next) => {
-  console.log("This ran!")
-  res.set("Access-Control-Allow-Origin", "*")
-  next();
-})
+// server.use((req, res, next) => {
+//   console.log("This ran!")
+//   res.set("Access-Control-Allow-Origin", "*")
+//   next();
+// })
 
 server.listen(port, () => {
   console.log(`listening on port ${port}`)
