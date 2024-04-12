2bcb5ab2bc2c03adb7afd96a40514ecdb5726cc2
Christian Anderson <tehninjawaffle@gmail.com>
2024-04-11 21:56:09 -0700

    Developed the functionality of the game, it is currently working in a vaccum, but I will have timers for it to recall itself to change the position in the future.

--- client/src/Components/secondSecret/secondSecret.js
+++ client/src/Components/secondSecret/secondSecret.js
@@ -4,15 +4,41 @@ import axios from "axios"
 import { useNavigate } from "react-router-dom"
 function SecondSecret() {
 
-  let lineOne = [0, 0, 1, 0, 0]
-  let lineTwo = [1, 0, 0, 0, 0]
-  let lineThree = [0, 0, 0, 1, 0]
+  let lineOne = [0, 0, 0, 0, 0]
+  let lineTwo = [0, 0, 0, 0, 0]
+  let lineThree = [0, 0, 0, 0, 0]
+
+  let initialLine = [lineOne, lineTwo, lineThree]
 
   let [message, setMessage] = useState({message: ""})
   let [score, setScore] = useState(0)
-  let [lines, setLines] = useState([lineOne, lineTwo, lineThree])
+  let [lines, setLines] = useState(initialLine)
   const navigate = useNavigate();
   
+  let setAdam = () => {
+
+    setLines(initialLine)
+
+    let randomLine = Math.floor(Math.random() * (2 - 0 + 1) + 0);
+    let randomNumber = Math.floor(Math.random() * (4 - 0 + 1) + 0);
+    let newLines = initialLine
+    console.log(newLines)
+    console.log("this is the randomLine:", randomLine, "this is the random number:", randomNumber)
+    newLines[randomLine][randomNumber] = 1
+    setLines([
+      newLines
+    ])
+    // console.log("this is lines:", lines)
+    // console.log("this is the line in lines:", lines[randomLine])
+    // console.log("this is the line in the line in the lines:", lines[randomLine][randomNumber])
+    // setTimeout(() => {
+    //   setLines(initialLine)
+    // }, 100)
+  }
+
+  let startGame = () => {
+    setAdam()
+  }
   let onclick = (event) => {
     axios.put("https://lock-and-key-server.onrender.com/api/auth/increase", { "username": sessionStorage.getItem("username") })
     .then(res => {
@@ -27,24 +53,32 @@ function SecondSecret() {
     })
   }
 
+
+  let boop = (event) => {
+    setScore(score + 1);
+    setAdam()
+  }
+  console.log(lines)
   return (
     <>
     <div className="gameTwoMain">
-      <div className="gameTwoExplanation">...Explanation</div>
+      <div className="gameTwoExplanation">We just got info that Adam Sandler is making Jack & Jill 2! Smash his face 20 times to to make that stupid idea go away!</div>
       <div className="gameTwoScore">
         <strong>Score:</strong>
         <b>{score}</b>
       </div>
       <div className="gameTwoButton">
-        <input type="button" value="start" />
+        <input type="button" value="start" onClick={() => startGame()} />
       </div>
       <div className="gameTwoBoard">
         {lines.map((line) => {
-          console.log("this is the line", line)
+          // console.log("this is the line", line)
+          console.log(line)
           return (
           line.map((num) => {
+            console.log(num)
             return (
-              num === 0 ? <div className="gameTwoHole">{num}</div> : <div className="gameTwoHole gopher">{num}</div>
+              num === 0 ? <div className="gameTwoHole">{num}</div> : <div className="gameTwoHole gopher" onClick={(event) => {boop(event)}}>{num}</div>
           )
           }))
         })}
