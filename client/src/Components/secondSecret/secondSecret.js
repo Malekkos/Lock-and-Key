1fee35078584828da7dc41e1e8806211050c694c
Christian Anderson <tehninjawaffle@gmail.com>
2024-04-15 11:45:03 -0700

    Well, I found the issue. When I was setting state, I had included a pair of [] brackets in the pool. This would then enshroud the return value in a new set of brackets, and that was why I was getting mutations when starting the game. Should be able to finally start working correctly on this

--- client/src/Components/secondSecret/secondSecret.js
+++ client/src/Components/secondSecret/secondSecret.js
@@ -3,37 +3,30 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
 function SecondSecret() {
+  let lineOne = [0, 0, 0, 0, 0]
+  let lineTwo = [0, 0, 0, 0, 0]
+  let lineThree = [0, 0, 0, 0, 0]
   
-  let lineOne = [[0], [0], [0], [0], [0]]
-  let lineTwo = [[0], [0], [0], [0], [0]]
-  let lineThree = [[0], [0], [0], [0], [0]]
-
-  let initialLine = [lineOne, lineTwo, lineThree]
+  let initialLine = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
 
 
   let [message, setMessage] = useState({ message: "" })
   let [score, setScore] = useState(0)
-  let [lines, setLines] = useState(initialLine)
+  let [lines, setLines] = useState([lineOne, lineTwo, lineThree])
   const navigate = useNavigate();
 
-  let setAdam = (event, starting) => {
-    console.log(event)
-    setLines(initialLine)
-    // let lineChange = document.get
-    if(starting) {
-      
-    }
+  let setAdam = (event) => {
     let randomLine = Math.floor(Math.random() * (2 - 0 + 1) + 0);
     let randomNumber = Math.floor(Math.random() * (4 - 0 + 1) + 0);
     let newLines = initialLine
     newLines[randomLine][randomNumber] = 1
-    setLines([
+    setLines(
       newLines
-    ])
+    )
   }
 
   let startGame = (event) => {
-    setAdam(event, "start")
+    setAdam(event)
   }
   let onclick = (event) => {
     axios.put("https://lock-and-key-server.onrender.com/api/auth/increase", { "username": sessionStorage.getItem("username") })
@@ -51,7 +44,7 @@ function SecondSecret() {
 
 
   let boop = (event) => {
-    console.log(event)
+    // console.log(event)
     setScore(score + 1);
     if(score === 20) {
       console.log("You win!")
@@ -72,17 +65,16 @@ function SecondSecret() {
           <input type="button" value="start" onClick={(event) => startGame(event)} />
         </div>
         <div className="gameTwoBoard">
-          {lines.map((line) => {
-            return (
-              line.map((num) => {
-                return (
-                  num.map((val, key) => {
-                    return (
-                      val === 0 ?<div className="gameTwoHole" key={key}>{val}</div> : <div className="gameTwoHole gopher" key={key} onClick={(event) => { boop(event) }}>{val}</div>
-                    )
-                  })
+          {lines.map((val) => {
+            console.log(val)
+            return(
+              val.map((line) => {
+                console.log(line)
+                return(
+                  line === 0 ? <div className="gameTwoHole">{line}</div> : <div className="gameTwoHole gopher">{line}</div>
                 )
-              }))
+              })
+            )
           })}
         </div>
       </div>
